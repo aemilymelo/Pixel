c568f20a3a7f04ebf2e723f018a7a2abe2302d16
Andrade kaique <kaique.andrade@consulting-for.edenred.com>
2025-05-28 08:27:17 -0300

    fix: correção cognito

--- src/main/java/br/edu/utfpr/api/dto/AuthRequest.java
+++ src/main/java/br/edu/utfpr/api/dto/AuthRequest.java
@@ -1,27 +1,13 @@
 package br.edu.utfpr.api.dto;
-import lombok.Data;
-import lombok.ToString;
 
 
-@Data
-@ToString
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AuthRequest {
     private String username;
     private String password;
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
